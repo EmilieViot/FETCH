0d612416a797af3729bc6829b8e547f4ca3e9e99
EmilieViot <149368279+EmilieViot@users.noreply.github.com>
2024-03-22 14:25:41 +0100

    adding first files

---
+++ controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+class DefaultController extends AbstractController
+{
+    public function home(): void
+    {
+        $this->render("default/home.html.twig", [
+            "title" => "Accueil"
+        ]);
+    }
+    public function notFound(): void
+    {
+        $this->render("default/404.html.twig", [
+            "title" => "404 : Page introuvable"
+        ]);
+    }
+}
---
+++ controllers/ShopController.php
@@ -0,0 +1,22 @@
+<?php
+
+class ShopController extends AbstractController
+{
+    public function shop(): void
+    {
+        $this->render("shop/shop.html.twig", [
+            "title" => "Boutique"
+        ]);
+    }
+
+    public function addToCart(): void
+    {
+        echo "ShopController->addToCart<br>";
+    }
+    public function cart(): void
+    {
+        $this->render("shop/cart.html.twig", [
+            "title" => "Panier"
+        ]);
+    }
+}
